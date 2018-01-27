8ad391e4bc0c01e0d048c8fa9486b65df2e7bdba
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-27 17:48:23 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+function ninjaBabySelector(){
+  return $('')
+}
+
+function divSelector(){
+  return $('')
+}
+
+function firstListItem(){
+  return
+}
